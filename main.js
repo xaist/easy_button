4a13cc884b15cfd804f523d8a6211d117163d605
xaist <xaistr@gmail.com>
2016-11-18 16:47:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ function sayThatWasEasy() {
   thatWasEasy.play();
 }
 $("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event){
+  if(event.charCode == 32){
+    $("#easy").trigger("click");
+  }
+}
